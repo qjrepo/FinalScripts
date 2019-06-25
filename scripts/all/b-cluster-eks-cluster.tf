69cf5f1e8fe510c8ad934901b08ed738e93f2de7
Qianjin Wu <wu.qian@husky.neu.edu>
2019-06-25 16:29:52 -0400

    uploaded scripts

---
+++ scripts/all/b-cluster-eks-cluster.tf
@@ -0,0 +1,20 @@
+#
+# EKS Cluster Resources
+#  * EKS Cluster
+#
+#  It can take a few minutes to provision on AWS
+
+resource "aws_eks_cluster" "demo" {
+  name     = "${var.cluster-name}"
+  role_arn = "${aws_iam_role.demo-cluster.arn}"
+
+  vpc_config {
+    security_group_ids = ["${aws_security_group.demo-cluster.id}"]
+    subnet_ids         = ["${aws_subnet.demo.*.id}"]
+  }
+
+  depends_on = [
+    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSClusterPolicy",
+    "aws_iam_role_policy_attachment.demo-cluster-AmazonEKSServicePolicy",
+  ]
+}
---
+++ scripts/all/c-nodes-eks-worker-nodes-asg.tf
@@ -0,0 +1,23 @@
+# usage of the specific kubernetes.io/cluster/* resource tag is required for 
+# EKS and Kubernetes to discover and manage compute resources
+
+resource "aws_autoscaling_group" "demo" {
+  desired_capacity     = 2
+  launch_configuration = "${aws_launch_configuration.demo.id}"
+  max_size             = 2
+  min_size             = 1
+  name                 = "qj1-eks-demo"
+  vpc_zone_identifier  = ["${aws_subnet.demo.*.id}"]
+
+  tag {
+    key                 = "Name"
+    value               = "qj1-eks-demo"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "kubernetes.io/cluster/${var.cluster-name}"
+    value               = "owned"
+    propagate_at_launch = true
+  }
+}
